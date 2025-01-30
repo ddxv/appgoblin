b59db8170869fe1846b9cf977c5bc92c6cd8baff
James O'Claire <jamesoclaire@gmail.com>
2025-01-30 17:32:48 +0800

    Working new page for publisher id

---
+++ frontend/src/lib/CompanyPubAppsTable.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import Pagination from './Pagination.svelte';
+
+	import { DataHandler } from '@vincjo/datatables/legacy/remote';
+	import type { State } from '@vincjo/datatables/legacy/remote';
+	import type { CompanyOverviewApps } from '../types';
+
+	export let entries_table: CompanyOverviewApps[];
+
+	const totalRows = entries_table.length;
+
+	const rowsPerPage = 100;
+
+	const handler = new DataHandler<CompanyOverviewApps>([], {
+		rowsPerPage: rowsPerPage,
+		totalRows: totalRows
+	});
+	const rows = handler.getRows();
+
+	handler.onChange((state: State) =>
+		Promise.resolve(
+			entries_table.slice(
+				0 + (state.pageNumber - 1) * state.rowsPerPage,
+				state.rowsPerPage * state.pageNumber
+			)
+		)
+	);
+
+	handler.invalidate();
+	console.log(`TABLE Company: ${totalRows}`);
+
+	$: firstRowInstalls = entries_table && entries_table.length > 0 && entries_table[0].installs > 0;
+
+	function formatNumber(num: number) {
+		return new Intl.NumberFormat('en-US').format(num);
+	}
+
+	function formatDate(date: string) {
+		return new Date(date).toLocaleDateString('en-US', {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric'
+		});
+	}
+</script>
+
+<div class="table-container space-y-4">
+	<div class="overflow-x-auto pl-0">
+		<table class="md:table table-hover md:table-compact table-auto w-full text-xs">
+			<thead>
+				<tr>
+					<th class="table-cell-fit"></th>
+					<th class="table-cell-fit">App</th>
+					<th class="table-cell-fit">
+						{#if firstRowInstalls}
+							Installs
+						{:else}
+							Ratings
+						{/if}
+					</th>
+					<th class="table-cell-fit">App-Ads.txt Last Crawled</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each $rows as row, index}
+					<tr class="px-0">
+						<td class="table-cell-fit">
+							{index + 1}
+						</td>
+						<td class="table-cell-fit">
+							<a href="/apps/{row.store_id}" style="cursor: pointer;">
+								{row.app_name}
+							</a>
+						</td>
+
+						<td class="table-cell-fit">
+							{#if firstRowInstalls}
+								{formatNumber(row.installs)}
+							{:else}
+								{formatNumber(row.rating_count)}
+							{/if}
+						</td>
+						<td class="table-cell-fit">
+							{formatDate(row.developer_domain_crawled_at)}
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+		<footer class="flex justify-between">
+			<!-- <RowCount {handler} /> -->
+			{#if totalRows > rowsPerPage}
+				<Pagination {handler} />
+			{/if}
+		</footer>
+	</div>
+</div>
