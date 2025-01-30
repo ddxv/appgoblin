1010da654d99cb6561bb2af802887d7ea1abc9d1
James O'Claire <jamesoclaire@gmail.com>
2025-01-30 10:59:21 +0800

    WIP add adstxt pages

---
+++ frontend/src/routes/(categorical)/companies/[domain]/app-adstxt/publisher/[publisher_id]/+page.server.ts
@@ -0,0 +1,49 @@
+import type { PageServerLoad } from './$types';
+
+export const ssr: boolean = true;
+export const csr: boolean = true;
+
+export const load: PageServerLoad = async ({ parent, params }) => {
+	const companyDomain = params.domain;
+	const publisher_id = params.publisher_id;
+
+	const publishers_apps = fetch(
+		`http://localhost:8000/api/companies/${companyDomain}/adstxt/publisher/${publisher_id}`
+	);
+
+	const { companyDetails, companyTree } = await parent();
+
+	console.log(`start load overview for company=${companyDomain}`);
+	try {
+		return {
+			companyDetails: companyDetails,
+			publishersApps: publishers_apps
+				.then((resp) => {
+					if (resp.status === 200) {
+						return resp.json();
+					} else if (resp.status === 404) {
+						console.log('Company Parent Categories Not found');
+						return 'Company Parent Categories Not Found';
+					} else if (resp.status === 500) {
+						console.log('API Server error');
+						return 'Backend Error';
+					}
+				})
+				.then(
+					(json) => json,
+					(error) => {
+						console.log('Uncaught error', error);
+						return 'Uncaught Error';
+					}
+				),
+			companyTree: companyTree,
+		};
+	} catch (error) {
+		console.error('Failed to load data:', error);
+		return {
+			results: {},
+			status: 500,
+			error: 'Failed to load company data'
+		};
+	}
+};
---
+++ frontend/src/routes/(categorical)/companies/[domain]/app-adstxt/publisher/[publisher_id]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import CompanyPubIDTable from '$lib/CompanyPubIDTable.svelte';
+	let { data } = $props();
+</script>
+
+<div>
+	<h1>Publisher ID: {page.params.publisher_id}</h1>
+</div>
+<div>
+	{#await data.publishersApps}
+		<loading class="text-center">Loading...</loading>
+	{:then publishersApps}
+		<CompanyPubIDTable entries_table={publishersApps} />
+	{/await}
+</div>
