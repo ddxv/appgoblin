a4a0d18d03ba179aa1cdb9d5961b1aa741d51edd
James O'Claire <jamesoclaire@gmail.com>
2025-01-31 13:17:54 +0800

    Faster loading of overview page

---
+++ backend/dbcon/sql/query_company_adstxt_publisher_id_apps_overview.sql
@@ -0,0 +1,31 @@
+WITH ranked_apps AS (
+    SELECT
+        store,
+        store_id,
+        app_name,
+        installs,
+        rating_count,
+        developer_id,
+        developer_name,
+        developer_domain_url,
+        relationship,
+        developer_domain_crawled_at,
+        ROW_NUMBER() OVER (PARTITION BY store, relationship ORDER BY rating_count DESC) as rn
+    FROM adstxt_entries_store_apps
+    WHERE
+        ad_domain_url = :ad_domain_url
+        AND publisher_id = :publisher_id
+)
+SELECT
+    store,
+    store_id,
+    app_name,
+    installs,
+    rating_count,
+    developer_id,
+    developer_name,
+    developer_domain_url,
+    relationship,
+    developer_domain_crawled_at
+FROM ranked_apps
+WHERE rn <= 10;
