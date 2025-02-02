a6eaede458fa369032b3a4884b12b7b50a1d1d6f
James O'Claire <jamesoclaire@gmail.com>
2025-02-02 13:47:51 +0800

    small changes to ui for sdks

--- backend/dbcon/sql/query_app_sdk_overview.sql
+++ backend/dbcon/sql/query_app_sdk_overview.sql
@@ -3,6 +3,6 @@ SELECT
     cao.company_domain,
     cao.category_slug
 FROM
-    companies_apps_overview AS cao
+    frontend.companies_apps_overview AS cao
 WHERE
     cao.store_id = :store_id
--- backend/dbcon/sql/query_sdks.sql
+++ backend/dbcon/sql/query_sdks.sql
@@ -14,7 +14,7 @@ FROM (
                 ORDER BY app_count DESC
             )
         AS row_num
-    FROM companies_version_details_count
+    FROM frontend.companies_version_details_count
 ) AS ranked
 WHERE row_num <= 100
 ORDER BY store ASC, app_count DESC;
