5a3b66bd959f1f90df47424f20f69dd1f3704b3b
James O'Claire <jamesoclaire@gmail.com>
2025-02-27 10:15:02 +0800

    Begin removing postcss

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,25 +1,13 @@
-import { join } from 'path';
 import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
 
-import { skeleton } from '@skeletonlabs/skeleton/plugin';
-import * as themes from '@skeletonlabs/skeleton/themes';
-
 const config = {
 	darkMode: 'selector',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton-svelte'), '../**/*.{html,js,svelte,ts}')
-	],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
-	plugins: [
-		forms,
-		skeleton({
-			themes: [themes.nouveau, themes.catppuccin]
-		})
-	]
+	plugins: [forms]
 } satisfies Config;
 
 export default config;
