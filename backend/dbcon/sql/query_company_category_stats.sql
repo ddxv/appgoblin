bbb296e1f06c520eb30701c3ecc7b28d63bea1af
James O'Claire <jamesoclaire@gmail.com>
2025-03-11 13:08:32 +0800

    Working total of monthly installs for company

---
+++ backend/dbcon/sql/query_company_category_stats.sql
@@ -0,0 +1,2 @@
+SELECT * FROM frontend.companies_category_stats
+WHERE company_domain = :company_domain;
---
+++ backend/dbcon/sql/query_company_parent_category_tag_stats.sql
@@ -0,0 +1,15 @@
+SELECT
+    company_domain,
+    company_name,
+    store,
+    tag_source,
+    app_category,
+    COALESCE(app_count, 0) AS app_count,
+    COALESCE(installs_total, 0) AS installs_total,
+    COALESCE(rating_count_total, 0) AS rating_count_total,
+    COALESCE(installs_d30, 0) AS installs_d30,
+    COALESCE(rating_count_d30, 0) AS rating_count_d30
+FROM
+    frontend.companies_parent_category_tag_stats
+WHERE company_domain = :company_domain
+;
