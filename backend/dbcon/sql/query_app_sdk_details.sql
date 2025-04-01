e1ad5ba2a1d268844850c3039eb0594766014334
James O'Claire <jamesoclaire@gmail.com>
2025-04-01 12:38:42 +0800

    Use new tables for version_strings

--- backend/dbcon/sql/query_app_sdk_details.sql
+++ backend/dbcon/sql/query_app_sdk_details.sql
@@ -1,11 +1,15 @@
 SELECT
-    cavd.xml_path,
-    cavd.value_name,
+    vs.xml_path,
+    vs.value_name,
     cavd.company_name,
     cavd.company_domain,
     cavd.category_slug
 FROM
-    frontend.companies_apps_version_details AS cavd
+    frontend.store_apps_version_details AS cavd
+LEFT JOIN
+    version_strings AS vs
+    ON
+        cavd.version_string_id = vs.id
 WHERE
     cavd.store_id = :store_id
 ORDER BY cavd.xml_path, cavd.value_name;
--- backend/dbcon/sql/query_sdk_pattern.sql
+++ backend/dbcon/sql/query_sdk_pattern.sql
@@ -1,6 +1,6 @@
 SELECT
-    cavd.xml_path,
-    cavd.value_name,
+    vs.xml_path,
+    vs.value_name,
     sa.store,
     sa.store_id,
     sa.name AS app_name,
@@ -8,11 +8,14 @@ SELECT
     sa.rating_count,
     sa.icon_url_512
 FROM
-    frontend.companies_apps_version_details AS cavd
+    frontend.store_apps_version_details AS cavd
 LEFT JOIN public.store_apps AS sa
     ON
         cavd.store_id = sa.store_id
+LEFT JOIN version_strings AS vs
+    ON
+        cavd.version_string_id = vs.id
 WHERE
-    cavd.value_name LIKE :value_pattern || '%'
+    vs.value_name LIKE :value_pattern || '%'
 ORDER BY COALESCE(sa.installs, sa.rating_count) DESC NULLS LAST
 LIMIT 200;
