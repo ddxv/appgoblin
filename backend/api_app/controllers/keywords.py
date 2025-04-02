b54730a6b978a5840d61f503a4dadb663f8b5542
James O'Claire <jamesoclaire@gmail.com>
2025-04-02 17:33:12 +0800

    Add description and keywords

---
+++ backend/api_app/controllers/keywords.py
@@ -0,0 +1,40 @@
+"""API for keywords data.
+
+/keywords/app/{store_id} keywords for a specific app
+"""
+
+import time
+from typing import Self
+
+from litestar import Controller, get
+
+
+from config import get_logger
+from dbcon.queries import (
+    get_single_app_keywords,
+)
+
+logger = get_logger(__name__)
+
+
+class KeywordsController(Controller):
+    """Controller holding all API endpoints for keywords."""
+
+    path = "/api/keywords"
+
+    @get(path="/app/{store_id:str}", cache=86400)
+    async def get_app_keywords(self: Self, store_id: str) -> dict:
+        """Handle GET request for a list of apps.
+
+        Returns
+        -------
+            A dictionary representation of the total counts
+
+        """
+        start = time.perf_counter() * 1000
+        keywords_df = get_single_app_keywords(store_id)
+        keywords_list = keywords_df["keyword_text"].tolist()
+        keywords_dict = {"keywords": keywords_list}
+        duration = round((time.perf_counter() * 1000 - start), 2)
+        logger.info(f"{self.path}/app/{store_id} took {duration}ms")
+        return keywords_dict
---
+++ backend/dbcon/sql/query_single_app_keywords.sql
@@ -0,0 +1,31 @@
+WITH latest_en_descriptions AS (
+    SELECT
+        DISTINCT ON
+        (store_app)
+        sad.store_app,
+        sad.id AS description_id
+    FROM
+        store_apps_descriptions sad
+    LEFT JOIN store_apps sa ON
+        sad.store_app = sa.id
+    WHERE
+        sad.language_id = 1
+        AND sa.store_id = :store_id
+    ORDER BY
+        sad.store_app ASC,
+        sad.updated_at DESC
+)
+SELECT
+    keyword_text
+FROM
+    description_keywords dk
+LEFT JOIN keywords k ON
+    dk.keyword_id = k.id
+WHERE
+    dk.description_id IN (
+        SELECT
+            description_id
+        FROM
+            latest_en_descriptions
+    )
+;
