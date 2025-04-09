7c480c842d953d398d117e7994eac9c0d2b66c68
James O'Claire <jamesoclaire@gmail.com>
2025-04-09 13:13:56 +0800

    Build first tanstack table in svelte 5 with skeleton

--- frontend/src/lib/AppKeywordsTable.svelte
+++ frontend/src/lib/AppKeywordsTable.svelte
@@ -1,61 +1,187 @@
-<script lang="ts">
-	import Pagination from './clientside/Pagination.svelte';
-	import ThSort from './clientside/ThSort.svelte';
+<script lang="ts" generics="TData, TValue">
+	import {
+		type ColumnDef,
+		type PaginationState,
+		type SortingState,
+		getCoreRowModel,
+		getPaginationRowModel,
+		getSortedRowModel
+	} from '@tanstack/table-core';
+	import { renderComponent } from '$lib/components/data-table/index.js';
+	import { mkConfig, generateCsv, download } from 'export-to-csv';
 
-	import { DataHandler } from '@vincjo/datatables/legacy';
+	import ColumnSortButton from '$lib/components/data-table/ColumnSortButton.svelte';
+
+	import Pagination from '$lib/components/data-table/Pagination.svelte';
+
+	import { createSvelteTable, FlexRender } from '$lib/components/data-table/index.js';
 	import type { KeywordScore } from '../types';
 
-	let { entries_table }: { entries_table: KeywordScore[] } = $props();
+	const columns: ColumnDef<KeywordScore>[] = [
+		{
+			accessorKey: 'keyword_text',
+			header: ({ column }) =>
+				renderComponent(ColumnSortButton, {
+					columnTitle: 'Keyword',
+					sortDirection: column.getIsSorted(),
+					onclick: () => {
+						const currentSort = column.getIsSorted();
+						if (currentSort === false) {
+							column.toggleSorting(false); // Set to ascending
+						} else if (currentSort === 'asc') {
+							column.toggleSorting(true); // Set to descending
+						} else {
+							column.clearSorting(); // Clear sorting (back to unsorted)
+						}
+					}
+				})
+		},
+		{
+			accessorKey: 'competitiveness_score',
+			header: ({ column }) =>
+				renderComponent(ColumnSortButton, {
+					columnTitle: 'Competitiveness Score',
+					sortDirection: column.getIsSorted(),
+					onclick: () => {
+						const currentSort = column.getIsSorted();
+						if (currentSort === false) {
+							column.toggleSorting(false); // Set to ascending
+						} else if (currentSort === 'asc') {
+							column.toggleSorting(true); // Set to descending
+						} else {
+							column.clearSorting(); // Clear sorting (back to unsorted)
+						}
+					}
+				})
+		},
+		{
+			accessorKey: 'd30_best_rank',
+			header: ({ column }) =>
+				renderComponent(ColumnSortButton, {
+					columnTitle: '30 Day Best Rank',
+					sortDirection: column.getIsSorted(),
+					onclick: () => {
+						const currentSort = column.getIsSorted();
+						if (currentSort === false) {
+							column.toggleSorting(false); // Set to ascending
+						} else if (currentSort === 'asc') {
+							column.toggleSorting(true); // Set to descending
+						} else {
+							column.clearSorting(); // Clear sorting (back to unsorted)
+						}
+					}
+				})
+		}
+	];
 
-	const totalRows = entries_table.length;
+	type DataTableProps<KeywordScore, TValue> = {
+		// columns: ColumnDef<TData, TValue>[];
+		data: KeywordScore[];
+	};
 
-	const rowsPerPage = 10;
+	let { data }: DataTableProps<KeywordScore, TValue> = $props();
+	let pagination = $state<PaginationState>({ pageIndex: 0, pageSize: 10 });
+	let sorting = $state<SortingState>([]);
 
-	const handler = new DataHandler<KeywordScore>(entries_table, {
-		rowsPerPage: rowsPerPage
+	const table = createSvelteTable({
+		get data() {
+			return data;
+		},
+		columns,
+		state: {
+			get pagination() {
+				return pagination;
+			},
+			get sorting() {
+				return sorting;
+			}
+		},
+
+		getSortedRowModel: getSortedRowModel(),
+		onSortingChange: (updater) => {
+			if (typeof updater === 'function') {
+				sorting = updater(sorting);
+			} else {
+				sorting = updater;
+			}
+		},
+
+		onPaginationChange: (updater) => {
+			if (typeof updater === 'function') {
+				pagination = updater(pagination);
+			} else {
+				pagination = updater;
+			}
+		},
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel()
 	});
 
-	const rows = handler.getRows();
+	const csvConfig = mkConfig({
+		fieldSeparator: ',',
+		filename: 'appgoblin_data', // export file name (without .csv)
+		decimalSeparator: '.',
+		useKeysAsHeaders: true
+	});
+
+	const exportDataCSV = (rows: Row<_>[]) => {
+		const rowData = rows.map((row) => row.original);
+		const csv = generateCsv(csvConfig)(rowData);
+		download(csvConfig)(csv);
+	};
 </script>
 
 <div class="table-container space-y-4">
 	<div class="overflow-x-auto pl-0">
 		<table class="md:table table-hover md:table-compact table-auto w-full text-xs">
 			<thead>
-				<tr>
-					<th class="table-cell-fit"> <ThSort {handler} orderBy="keyword_text">Keyword</ThSort></th>
-					<th class="table-cell-fit">
-						<ThSort {handler} orderBy="competitiveness_score">Competitiveness Score</ThSort></th
-					>
-					<th class="table-cell-fit"
-						><ThSort {handler} orderBy="d30_best_rank">30 Day Best Rank</ThSort></th
-					>
-				</tr>
+				{#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
+					<tr>
+						{#each headerGroup.headers as header (header.id)}
+							<th>
+								{#if !header.isPlaceholder}
+									<FlexRender
+										content={header.column.columnDef.header}
+										context={header.getContext()}
+									/>
+								{/if}
+							</th>
+						{/each}
+					</tr>
+				{/each}
 			</thead>
 			<tbody>
-				{#each $rows as row, index}
-					<tr class="px-0">
+				{#each table.getRowModel().rows as row (row.id)}
+					<tr>
 						<td class="table-cell-fit">
-							<a href="/keywords/en/{row.keyword_text}"> {row.keyword_text}</a>
-						</td>
-						<!-- <td class="table-cell-fit">
-							{row.total_apps}
-						</td> -->
-						<td class="table-cell-fit">
-							{row.competitiveness_score}
-							<p class="inline text-xs text-secondary-400-600">{row.app_count} apps</p>
+							<a href="/keywords/en/{row.original.keyword_text}"> {row.original.keyword_text}</a>
 						</td>
 						<td class="table-cell-fit">
-							{row.d30_best_rank}
+							{row.original.competitiveness_score}
+							<p class="inline text-xs text-secondary-400-600">{row.original.app_count} apps</p>
 						</td>
+						<td class="table-cell-fit">
+							{row.original.d30_best_rank}
+						</td>
+					</tr>
+				{:else}
+					<tr>
+						<td colspan={columns.length} class="h-24 text-center">No results.</td>
 					</tr>
 				{/each}
 			</tbody>
 		</table>
 		<footer class="flex justify-between">
-			{#if totalRows > rowsPerPage}
-				<Pagination {handler} />
-			{/if}
+			<div class="flex items-center justify-end space-x-2 py-4">
+				<Pagination tableModel={table} />
+				<button
+					type="button"
+					class="btn btn-sm preset-outlined-primary-100-900 p-0"
+					onclick={() => exportDataCSV(table.getFilteredRowModel().rows)}
+				>
+					Download CSV
+				</button>
+			</div>
 		</footer>
 	</div>
 </div>
