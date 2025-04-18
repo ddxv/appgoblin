5bb25af863e7a17b3101b1fe4365670ceb44d68d
James O'Claire <jamesoclaire@gmail.com>
2025-04-18 10:38:55 +0800

    speed up app rankings

--- backend/dbcon/sql/query_most_recent_top_ranks.sql
+++ backend/dbcon/sql/query_most_recent_top_ranks.sql
@@ -1,9 +1,10 @@
 WITH latest_crawled_date AS (
-  SELECT max(arr.crawled_date) AS crawled_date
-  FROM frontend.store_apps_rankings AS arr
-  JOIN countries AS c ON arr.country = c.id
-  WHERE arr.store_collection = :collection_id AND c.alpha2 = :country
+    SELECT max(arr.crawled_date) AS crawled_date
+    FROM frontend.store_apps_rankings AS arr
+    INNER JOIN countries AS c ON arr.country = c.id
+    WHERE arr.store_collection = :collection_id AND c.alpha2 = :country
 )
+
 SELECT
     ar.rank,
     sa.name,
@@ -11,8 +12,8 @@ SELECT
     sa.icon_url_512
 FROM
     frontend.store_apps_rankings AS ar
-JOIN latest_crawled_date lcd ON ar.crawled_date = lcd.crawled_date
-JOIN countries AS c ON ar.country = c.id
+INNER JOIN latest_crawled_date AS lcd ON ar.crawled_date = lcd.crawled_date
+INNER JOIN countries AS c ON ar.country = c.id
 LEFT JOIN store_apps AS sa ON ar.store_app = sa.id
 WHERE
     ar.store_collection = :collection_id
