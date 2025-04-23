13bdc0d85d3d5ba53db2b1736951dfdb224d1560
James O'Claire <jamesoclaire@gmail.com>
2025-04-23 16:27:36 +0800

    set the sveltekit port dynamically

--- frontend/src/routes/blog/+layout.server.ts
+++ frontend/src/routes/blog/+layout.server.ts
@@ -1,14 +1,14 @@
 export const csr = false;
 
-// import { PUBLIC_DOCS_HOST } from '$env/static/public';
+import { PUBLIC_BLOG_HOST } from '$env/static/public';
 
 async function fetchContent(url: URL) {
 	let htmlContent;
 	if (url && !url.pathname.includes('.')) {
 		// Ensure the path correctly points to where mkdocs HTML files were generated to.
 		// ie www/static/documentation/docs/index.html
-		const processedPathname = url.pathname.replace("", "");
-		var mypath = `http://localhost:5173/generated-blog${processedPathname}/index.html`;
+		const processedPathname = url.pathname.replace('', '');
+		var mypath = `${PUBLIC_BLOG_HOST}/generated-blog${processedPathname}/index.html`;
 		console.log(`Try rendering static blog path=${mypath}`);
 		const response = await fetch(mypath);
 		if (response.ok) {
