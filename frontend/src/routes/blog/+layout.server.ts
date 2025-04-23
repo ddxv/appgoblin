332331efd9611777f0ef8808575a7e2470ba56c3
James O'Claire <jamesoclaire@gmail.com>
2025-04-23 15:37:52 +0800

    Serve static astro html in sveltekit

---
+++ frontend/src/routes/blog/+layout.server.ts
@@ -0,0 +1,30 @@
+export const csr = false;
+
+// import { PUBLIC_DOCS_HOST } from '$env/static/public';
+
+async function fetchContent(url: URL) {
+	let htmlContent;
+	if (url && !url.pathname.includes('.')) {
+		// Ensure the path correctly points to where mkdocs HTML files were generated to.
+		// ie www/static/documentation/docs/index.html
+		const processedPathname = url.pathname.replace("", "");
+		var mypath = `http://localhost:5173/generated-blog${processedPathname}/index.html`;
+		console.log(`Try rendering static blog path=${mypath}`);
+		const response = await fetch(mypath);
+		if (response.ok) {
+			htmlContent = await response.text();
+		} else {
+			htmlContent = 'Page not found.';
+		}
+	}
+	return htmlContent;
+}
+
+export async function load({ url }: { url: URL }) {
+	const myhtml = await fetchContent(url);
+	return {
+		props: {
+			myblog: myhtml
+		}
+	};
+}
---
+++ frontend/src/routes/blog/[...catchall]/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	let { data } = $props();
+
+	let title = 'Blog | OpenAttribution Latest News';
+	const description =
+		'Latest news and updates from the OpenAttribution team. Learn about the latest features, updates, and improvements.';
+	const keywords =
+		'blog, openattribution, open source, mmp, mobile, ad tracking, ad analysis, ad platform';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta property="og:title" content={title} />
+	<meta name="twitter:title" content={title} />
+	<meta name="description" content={description} />
+	<meta name="keywords" content={keywords} />
+	<meta property="og:description" content={description} />
+	<meta name="twitter:description" content={description} />
+</svelte:head>
+
+<iframe
+	srcdoc={data.props.myblog}
+	class="h-screen w-full"
+	frameborder="0"
+	title="Blog Content"
+	id="frame"
+	style="color-scheme: dark;"
+></iframe>
