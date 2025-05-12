38dd0389f9477e9a738eb4cba8901123a7045d19
James O'Claire <jamesoclaire@gmail.com>
2025-05-12 09:40:53 +0800

    added request scan button for api calls section

--- frontend/src/routes/apps/[id]/(sub)/data-flows/+page.server.ts
+++ frontend/src/routes/apps/[id]/(sub)/data-flows/+page.server.ts
@@ -1,9 +1,26 @@
 import type { PageServerLoad } from './$types.js';
 
-export const load: PageServerLoad = async ({ params, parent }) => {
+import type { Actions } from './$types';
+
+export const actions = {
+	requestSDKScan: async (event) => {
+		const formData = await event.request.formData();
+		const appId = formData.get('appId');
+		console.log('requestSDKScan', appId);
+
+		const response = await fetch(`http://localhost:8000/api/apps/${appId}/requestSDKScan`, {
+			method: 'POST'
+		});
+		if (response.status === 200) {
+			return { success: true };
+		} else {
+			return { success: false };
+		}
+	}
+} satisfies Actions;
+
+export const load: PageServerLoad = async ({ params }) => {
 	const id = params.id;
-	// Load parent data first because it is cached
-	const { companyTypes } = await parent();
 
 	console.log('id', id);
 
--- frontend/src/routes/apps/[id]/(sub)/data-flows/+page.svelte
+++ frontend/src/routes/apps/[id]/(sub)/data-flows/+page.svelte
@@ -3,20 +3,25 @@
 	import AppAPICallsTable from '$lib/AppAPICallsTable.svelte';
 
 	import type { AppAPIsOverview } from '../../../../../types';
+	import RequestSDKScanButton from '$lib/RequestSDKScanButton.svelte';
 
 	let { data }: { data: AppAPIsOverview } = $props();
 </script>
 
 <div class="card preset-tonal p-2 md:p-16 mt-2 md:mt-4">
 	<h4 class="h4 md:h3 p-2">API Calls</h4>
-	This is a stripped down overview of the API calls recorded after installing the app for the first time
-	and opening the app for 1 minute.
+
 	{#await data.apis}
 		Loading API data...
 	{:then apis}
-		{#if typeof apis == 'string'}
-			<p>API data not yet available for this app.</p>
+		{#if typeof apis == 'string' || apis.apis.length == 0}
+			<p>API calls data not yet available for this app.</p>
+			<RequestSDKScanButton />
 		{:else if apis.apis && Object.keys(apis.apis).length > 0}
+			<p>
+				This is a stripped down overview of the API calls recorded after installing the app for the
+				first time and opening the app for 1 minute.
+			</p>
 			<WhiteCard>
 				{#snippet title()}
 					APIs
