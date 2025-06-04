8786f21d101fab96a4875ac10c850602a6b934e5
James O'Claire <jamesoclaire@gmail.com>
2025-06-04 10:44:18 +0800

    Clean up app page on full size

--- frontend/src/routes/apps/[id]/+layout.svelte
+++ frontend/src/routes/apps/[id]/+layout.svelte
@@ -118,9 +118,9 @@
 		</div>
 	{:then myapp}
 		<h1 class="text-primary-900-100 text-3xl font-bold mb-4">{myapp.name}</h1>
-		<div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6">
 			<!-- COL 1  -->
-			<div class="grid grid-cols-3 md:grid-cols-1 gap-0">
+			<div class="grid grid-cols-3 gap-0">
 				<div class="col-span-1">
 					{#if myapp.icon_url_512}
 						<img
