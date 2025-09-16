0dce8fb256765ed71723e47acdab2e0b9ca2a6e6
James O'Claire <jamesoclaire@gmail.com>
2025-09-16 15:59:38 +0800

    No serverside render for categories

--- frontend/src/routes/(categorical)/+layout.server.ts
+++ frontend/src/routes/(categorical)/+layout.server.ts
@@ -2,6 +2,8 @@ import type { LayoutServerLoad } from './$types';
 
 import { getCachedData } from '../../hooks.server';
 
+export const csr = false;
+
 export const load: LayoutServerLoad = async () => {
 	const cachedData = await getCachedData();
 	return {
