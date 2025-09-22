cdac32e7b5bbb7ccbb7135d9da5527be99c62c80
James O'Claire <jamesoclaire@gmail.com>
2025-09-22 16:33:22 +0800

    Semi working CSS

--- frontend/src/routes/blog/[id]/+page.svelte
+++ frontend/src/routes/blog/[id]/+page.svelte
@@ -2,6 +2,15 @@
 	let { data } = $props();
 </script>
 
-<article class="prose prose-lg mx-auto p-4">
+<!-- <article class="prose prose-lg mx-auto p-4 post"> -->
+<article class="mx-48 p-8 post">
 	{@html data.contentHTML}
 </article>
+
+<!-- <style>
+	.post :global {
+		h2 {
+			@apply !h2;
+		}
+	}
+</style> -->
