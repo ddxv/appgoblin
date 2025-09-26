7ca5ab0f3155bdd36d0275e9bfb5950e16222307
James O'Claire <jamesoclaire@gmail.com>
2025-09-26 12:29:02 +0800

    Remove await on company category

--- frontend/src/routes/(categorical)/companies/[domain]/[category]/+page.svelte
+++ frontend/src/routes/(categorical)/companies/[domain]/[category]/+page.svelte
@@ -15,48 +15,30 @@
 	let company_category = $derived(page.params.category!);
 </script>
 
-{#await data.companyDetails}
-	<div class="p-6 rounded-lg shadow-md flex justify-center items-center h-40">
-		<span class="text-lg">Loading company info...</span>
-	</div>
-{:then myData}
-	<CompaniesLayout>
-		{#snippet card1()}
-			<WhiteCard>
-				{#snippet title()}
-					Totals
-				{/snippet}
-				{#if myData.categories[company_category]}
-					<TotalsBox
-						myTotals={myData.categories[company_category]}
-						myType={{ name: 'All Companies & Domains', url_slug: 'all-companies' }}
-					/>
-				{:else}
-					<p>
-						No apps found for
-						<span class="text-primary-900-100 font-bold">{company_category}</span>
-						category
-					</p>
-				{/if}
-			</WhiteCard>
-		{/snippet}
-	</CompaniesLayout>
-{:catch error}
-	<p class="text-red-500 text-center">{error.message}</p>
-{/await}
+<CompaniesLayout>
+	{#snippet card1()}
+		<WhiteCard>
+			{#snippet title()}
+				Totals
+			{/snippet}
+			{#if data.companyDetails.categories[company_category]}
+				<TotalsBox
+					myTotals={data.companyDetails.categories[company_category]}
+					myType={{ name: 'All Companies & Domains', url_slug: 'all-companies' }}
+				/>
+			{:else}
+				<p>
+					No apps found for
+					<span class="text-primary-900-100 font-bold">{company_category}</span>
+					category
+				</p>
+			{/if}
+		</WhiteCard>
+	{/snippet}
+</CompaniesLayout>
 
-{#await data.companyDetails}
-	<div><span>Loading company apps...</span></div>
-{:then detailsData}
-	{#await data.companyCategoryApps}
-		<div><span>Loading apps ...</span></div>
-	{:then tableData}
-		{#if typeof tableData == 'string'}
-			Failed to load company's apps.
-		{:else}
-			<CompanyTableGrid {tableData} {detailsData} category={company_category} />
-		{/if}
-	{:catch error}
-		<p style="color: red">{error.message}</p>
-	{/await}
-{/await}
+<CompanyTableGrid
+	tableData={data.companyCategoryApps}
+	detailsData={data.companyDetails}
+	category={company_category}
+/>
