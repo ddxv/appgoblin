36d0a7303706bd31754a43ac52f8376e818e3b59
James O'Claire <jamesoclaire@gmail.com>
2025-10-03 10:56:28 +0800

    Drop workers because hostinger cant handle it

--- backend/api_app/controllers/apps.py
+++ backend/api_app/controllers/apps.py
@@ -548,7 +548,8 @@ class AppController(Controller):
 
         """
         start = time.perf_counter() * 1000
-        df_overview = get_ranks_for_app_overview(state, store_id=store_id, days=90)
+        #df_overview = get_ranks_for_app_overview(state, store_id=store_id, days=90)
+        df_overview = pd.DataFrame()
         if df_overview.empty:
             logger.info(f"No ranks found for {store_id!r}")
             return AppRankOverview(countries=[], best_ranks=[])
@@ -577,7 +578,8 @@ class AppController(Controller):
 
         """
         start = time.perf_counter() * 1000
-        df = get_ranks_for_app(state, store_id=store_id, country=country, days=90)
+        #df = get_ranks_for_app(state, store_id=store_id, country=country, days=90)
+        df = pd.DataFrame()
         if df.empty:
             logger.info(f"No ranks found for {store_id!r}")
             return AppRank(history={})
