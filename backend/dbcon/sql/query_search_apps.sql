e3c90d2d5ef06f3e98c240d662ffc34405f86cea
James O'Claire <james@oclaire.dev>
2025-11-24 10:46:40 +0800

    use overview table for search queries

--- backend/dbcon/sql/query_search_apps.sql
+++ backend/dbcon/sql/query_search_apps.sql
@@ -6,38 +6,34 @@
 -- https://github.com/sqlfluff/sqlfluff/issues/4837
 WITH apps AS (
     SELECT *
-    FROM store_apps
-    WHERE textsearchable_index_col @@ to_tsquery('simple', :searchinput)
+    FROM frontend.store_apps_overview
+    WHERE textsearchable @@ to_tsquery('simple', :searchinput)
 ),
 ranked_apps AS (
     SELECT
         a.*,
-        agml.installs,
-        agml.rating,
-        agml.rating_count,
         row_number() OVER (
             PARTITION BY a.store
             ORDER BY
-                coalesce(agml.installs, agml.rating_count, 0) DESC NULLS LAST
+                coalesce(a.installs, a.rating_count, 0) DESC NULLS LAST
         ) AS store_rank,
         row_number() OVER (
             ORDER BY
                 a.store,
-                coalesce(agml.installs, agml.rating_count, 0) DESC NULLS LAST
+                coalesce(a.installs, a.rating_count, 0) DESC NULLS LAST
         ) % (SELECT count(DISTINCT store) FROM apps) AS round_robin_rank
     FROM apps AS a
-    LEFT JOIN app_global_metrics_latest AS agml ON a.id = agml.store_app
 )
 SELECT
     store,
-store_id,
-name,
-icon_url_100,
-icon_url_512,
+    store_id,
+    name,
+    icon_url_100,
+    icon_url_512,
     featured_image_url,
-installs,
-rating,
-rating_count
+    installs,
+    rating,
+    rating_count
 FROM ranked_apps
 ORDER BY store_rank, round_robin_rank
 LIMIT :mylimit;
