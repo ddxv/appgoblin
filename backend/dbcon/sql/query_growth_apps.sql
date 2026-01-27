666e59c86f61d1897f5851162350100da8a8bd10
James O'Claire <james@oclaire.dev>
2026-01-27 14:33:38 +0800

    Guarantee NULLs dont end up in final results

--- backend/dbcon/sql/query_growth_apps.sql
+++ backend/dbcon/sql/query_growth_apps.sql
@@ -3,5 +3,8 @@ FROM frontend.store_apps_z_scores
 WHERE
     store = :store
     AND (app_category = :app_category OR :app_category IS NULL)
-ORDER BY COALESCE(installs_z_score_2w, ratings_z_score_2w) DESC
+ORDER BY
+    COALESCE(
+        COALESCE(installs_z_score_2w, 0), COALESCE(ratings_z_score_2w, 0)
+    ) DESC
 LIMIT 100;
