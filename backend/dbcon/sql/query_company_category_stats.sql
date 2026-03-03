7d8516642858f0091840cefceceedaebcef96f82
James O'Claire <james@oclaire.dev>
2026-03-03 11:11:48 +0800

    Explicit list columns

--- backend/dbcon/sql/query_company_category_stats.sql
+++ backend/dbcon/sql/query_company_category_stats.sql
@@ -1,2 +1,10 @@
-SELECT * FROM frontend.companies_category_stats
+SELECT
+    store,
+    app_category,
+    company_domain,
+    company_name,
+    app_count,
+    installs_total,
+    installs_d30
+FROM frontend.companies_category_stats
 WHERE company_domain = :company_domain;
