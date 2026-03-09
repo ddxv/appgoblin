dece72f2d2e30dc002d5fcd93909929de148ead7
James O'Claire <james@oclaire.dev>
2026-03-09 16:02:50 +0800

    Id was missing from explicit columns in query

--- backend/dbcon/sql/query_countries.sql
+++ backend/dbcon/sql/query_countries.sql
@@ -1,7 +1,7 @@
 WITH countries_with_ranks AS (
     SELECT
         cs.name AS scenario_name,
-        country_id
+        cscc.country_id
     FROM crawl_scenario_country_config AS cscc
     LEFT JOIN crawl_scenarios AS cs ON cscc.scenario_id = cs.id
     WHERE cs.name = 'app_ranks'
@@ -10,21 +10,22 @@ WITH countries_with_ranks AS (
 countries_with_details AS (
     SELECT
         cs.name AS scenario_name,
-        country_id
+        cscc.country_id
     FROM crawl_scenario_country_config AS cscc
     LEFT JOIN crawl_scenarios AS cs ON cscc.scenario_id = cs.id
     WHERE cs.name = 'app_details'
 )
 
 SELECT
+    c.id,
     c.alpha2,
     c.langen,
     coalesce(
-        c.id IN (SELECT country_id FROM countries_with_ranks),
+        c.id IN (SELECT cwr.country_id FROM countries_with_ranks AS cwr),
         FALSE
     ) AS app_ranks,
     coalesce(
-        c.id IN (SELECT country_id FROM countries_with_details),
+        c.id IN (SELECT cwd.country_id FROM countries_with_details AS cwd),
         FALSE
     ) AS app_details
 FROM countries AS c;
