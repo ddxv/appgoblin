19c4b9e62c4c5b67c07fac45217b420a624db2e5
James O'Claire <james@oclaire.dev>
2026-03-19 15:10:48 +0800

    Add blog post about free datasets

--- frontend/src/routes/free-app-datasets/+page.server.ts
+++ frontend/src/routes/free-app-datasets/+page.server.ts
@@ -1,8 +1,6 @@
 import type { PageServerLoad } from './$types.js';
 import { createApiClient } from '$lib/server/api';
 
-export const prerender = true;
-
 export const load: PageServerLoad = async ({ fetch }) => {
 	const api = createApiClient(fetch);
 	const datasets = await api.get('/public/exports/datasets', 'Export Datasets');
