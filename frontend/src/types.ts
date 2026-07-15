b87ac1336bb32d454e5d0af1236e990ba6c6545c
James O'Claire <james@oclaire.dev>
2026-07-15 14:13:56 +0800

    rerun lint

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -619,15 +619,15 @@ export interface CompanyCategoryOverview {
 	domain_overview?: CompanyOverviewScope | null;
 	parent_overview?: CompanyOverviewScope | null;
 	mediation_adapters:
-	| {
-		adapter_string: string;
-		adapter_company_domain: string;
-		adapter_company_name: string;
-		adapter_logo_url: string;
-		app_category: string;
-		app_count: number;
-	}[]
-	| null;
+		| {
+				adapter_string: string;
+				adapter_company_domain: string;
+				adapter_company_name: string;
+				adapter_logo_url: string;
+				app_category: string;
+				app_count: number;
+		  }[]
+		| null;
 	categories: {
 		[key: string]: CategoryAppStats;
 	};
