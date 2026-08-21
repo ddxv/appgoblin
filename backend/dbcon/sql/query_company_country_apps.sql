e605e745cc0b1bb3440f9a1d0786319065ac1ac1
James O'Claire <james@oclaire.dev>
2026-08-21 09:56:28 +0800

    lint

--- backend/dbcon/sql/query_company_country_apps.sql
+++ backend/dbcon/sql/query_company_country_apps.sql
@@ -1,32 +1,34 @@
 WITH ranked_countries AS (
-	SELECT
-		store,
-		country,
-		app_count,
-		DENSE_RANK() OVER (
-			PARTITION BY domain_name, store
-			ORDER BY app_count DESC, country ASC
-		) AS rnk
-	FROM frontend.company_category_country_stats
-	WHERE domain_name = :company_domain
-		AND (app_category LIKE :app_category OR :app_category IS NULL)
+    SELECT
+        store,
+        country,
+        app_count,
+        DENSE_RANK() OVER (
+            PARTITION BY domain_name, store
+            ORDER BY app_count DESC, country ASC
+        ) AS rnk
+    FROM frontend.company_category_country_stats
+    WHERE
+        domain_name = :company_domain
+        AND (app_category LIKE :app_category OR :app_category IS NULL)
 )
+
 SELECT
-	store,
-	CASE
-		WHEN rnk <= 5 THEN country
-		ELSE 'Others'
-	END AS country,
-	SUM(app_count) AS app_count
+    store,
+    CASE
+        WHEN rnk <= 5 THEN country
+        ELSE 'Others'
+    END AS country,
+    SUM(app_count) AS app_count
 FROM ranked_countries
 GROUP BY
-	store,
-	CASE
-		WHEN rnk <= 5 THEN country
-		ELSE 'Others'
-	END
+    store,
+    CASE
+        WHEN rnk <= 5 THEN country
+        ELSE 'Others'
+    END
 ORDER BY
-	store,
-	-- Places the 'other' row at the bottom of each group
-	MIN(rnk) ASC,
-	app_count DESC;
+    store ASC,
+    -- Places the 'other' row at the bottom of each group
+    MIN(rnk) ASC,
+    app_count DESC;
